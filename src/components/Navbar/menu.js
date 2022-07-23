2a9947408b76a91877389029dd40db5789da8983
Rishit Shivesh <rishitshivesh@gmail.com>
2022-07-23 22:36:54 +0530

    Update menu.js

--- src/components/Navbar/menu.js
+++ src/components/Navbar/menu.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { StyledMenu } from "./menu.styled";
-
+import { Link } from "react-router-dom";
 const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
-      <a href="/">Home</a>
+      <Link to="/">Home</Link>
       <a href="/">About</a>
       <a href="/">Division</a>
       <a href="/">Projects</a>
-      <a href="/">Team</a>
+      <Link
+        to="/team"
+        className="text-gray-600 hover:text-[#ef6522] transition"
+      >
+        Team
+      </Link>
       <a href="/">Contact</a>
     </StyledMenu>
   );
