c4926664c03115f9712b6cbed0dc054b3ba511de
Vinay Sonkusale <sonkusalevinay20.20@gmail.com>
2022-08-02 10:55:00 +0530

    section 3 complete

--- src/components/Section3/Section3.js
+++ src/components/Section3/Section3.js
@@ -1,6 +1,8 @@
 import Heading from "../heading";
 import Card from "./cards";
 import data from "../../data/whatwedo.json";
+import macbook from "../../assets/images/whatwedo/macbook.png";
+import iphone from "../../assets/images/whatwedo/iphone.png";
 import Tilt from "react-vanilla-tilt";
 
 export default function Section3() {
@@ -17,8 +19,13 @@ export default function Section3() {
           mt="0"
         />
         <div className="flex flex-row justify-between">
-            <div className="md:basis-1/2 md:flex hidden">
-                
+            <div className="md:basis-1/2 flex-col justify-start md:flex hidden">
+                <img className="w-[30vw]" src={macbook}></img>
+                <div className=" flex flex-row shadow-0 justify-start -ml-[10vh] -mt-5">
+                    <img src={iphone}></img>
+                    <img className="-ml-[20vh] " src={iphone}></img>
+
+                </div>
             </div>
         <div className=" flex flex-wrap justify-around align-right px-7 md:basis-1/2 basis-full py-5 gap-x-2">
         {Object.keys(data).map((card)=>{
