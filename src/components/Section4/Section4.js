ee2bf716891ab7fda6826ae97d6980ec18e810cf
VikashPR <vikashrajendran2806@gmail.com>
2022-09-03 10:12:22 +0530

    fix(featuredProject): id update

--- src/components/Section4/Section4.js
+++ src/components/Section4/Section4.js
@@ -101,7 +101,7 @@ export default function Section4() {
       </div>
 
       <div
-        id="carouselExampleCrossfade"
+        id="featuredProjectsFade"
         className="carousel slide carousel-fade relative px-2 py-2 mx-auto lg:pt-5 lg:px-20 lg:hidden md:hidden"
         data-bs-ride="carousel"
       >
@@ -128,7 +128,7 @@ export default function Section4() {
         <button
           class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-4"
           type="button"
-          data-bs-target="#carouselExampleCrossfade"
+          data-bs-target="#featuredProjectsFade"
           data-bs-slide="prev"
         >
           <span class="inline-block bg-no-repeat" aria-hidden="true">
@@ -153,7 +153,7 @@ export default function Section4() {
         <button
           class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-4"
           type="button"
-          data-bs-target="#carouselExampleCrossfade"
+          data-bs-target="#featuredProjectsFade"
           data-bs-slide="next"
         >
           <span class="inline-block bg-no-repeat" aria-hidden="true">
