3a40f11bbbe75be3833616889971e5bc77e62de4
Rishit Shivesh <rishitshivesh@gmail.com>
2022-09-23 17:46:33 +0530

    Update Data

--- src/components/Section3/cards.js
+++ src/components/Section3/cards.js
@@ -1,25 +1,22 @@
 import Tilt from "react-vanilla-tilt";
 import styles from "./cards.module.css";
-export default function card({image,heading,description}){
+export default function card({ image, heading, description }) {
+  const img = require("../../assets/images/whatwedo/" + image);
+  console.log(img);
 
-    const img = require('../../assets/images/whatwedo/'+image);
-    console.log(img)
-    
-    return(
-        <Tilt className={styles.box}>
-        <div className="bg-[#ffffff10] w-[200px] h-[35vh] hover:cursor-pointer hover:scale-[1.02] hover:shadow-lg transition flex flex-col justify-center rounded-3xl group hover:bg-[#EF652230] ">
-            
-        <div className="w-[70px] h-[70px] flex justify-center bg-[#EF6522] mx-auto rounded-full">
-            <img src={img} className="rounded-full w-[40px] mx-auto"/>
-        </div>
-        <p className="text-center font-black text-xl mt-2 text-[#EF6522]">
+  return (
+    <Tilt className={styles.box}>
+      <div className="bg-[#ffffff10] w-[200px] h-[35vh] hover:cursor-pointer hover:scale-[1.02] hover:shadow-lg transition flex flex-col justify-between py-6 px-2 rounded-3xl group hover:bg-[#EF652230] ">
+        <div>
+          <div className="w-[70px] h-[70px] flex justify-center bg-[#EF6522] mx-auto rounded-full">
+            <img src={img} className="rounded-full w-[40px] mx-auto" />
+          </div>
+          <p className="text-center font-black text-xl mt-2 text-[#EF6522]">
             {heading}
-        </p>
-        <p className="text-center mx-[10px]">
-            {description}
-        </p>
-        
+          </p>
         </div>
-        </Tilt>
-    )
+        <p className="text-center mx-[10px]">{description}</p>
+      </div>
+    </Tilt>
+  );
 }
