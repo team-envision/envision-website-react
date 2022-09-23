a0e03a90df68144075d02a55dd0a96562d3456a5
Rishit Shivesh <rishitshivesh@gmail.com>
2022-09-23 18:30:53 +0530

    .

--- src/components/Team/Card.js
+++ src/components/Team/Card.js
@@ -13,10 +13,10 @@ export default function Card({ user }) {
     // <div>
     <div className=" w-[300px] md:w-[350px] h-[180px] rounded-3xl p-5 py-7 font-[roboto]  bg-white flex flex-row relative overflow-visible group transition duration-300 hover:shadow-xl lg:hover:scale-[1.02] my-3 overflow-x-hidden overflow-y-visible">
       <div className="flex flex-col max-w-[220px] align-middle my-auto">
-        <p className=" text-[1.2rem] md:text-[1.3rem] group-hover:text-[#EF6522] transition max-w-[180px]">
+        <p className=" text-[1rem] md:text-[1.3rem] group-hover:text-[#EF6522] transition max-w-[170px] md:max-w-[180px]">
           {user.name}
         </p>
-        <p className=" text-[1rem] md:text-[1.05rem] text-[#bbb]">
+        <p className=" text-[0.95rem] md:text-[1.05rem] text-[#bbb]">
           {user.domain}
         </p>
         <div className="flex flex-row mt-5 text-xl md:gap-x-4 gap-x-2">
@@ -72,7 +72,7 @@ export default function Card({ user }) {
         </div> */}
       <img
         src={user.image}
-        className="w-[150px] md:w-[170px] absolute right-0 bottom-[1px] translate-x-3 duration-300 saturate-0 group-hover:saturate-100 transition hover:cursor-pointer group-hover:scale-[1.1] group-hover:translate-y-[-0.9vh]"
+        className="w-[145px] md:w-[170px] absolute right-0 bottom-[1px] translate-x-3 duration-300 saturate-0 group-hover:saturate-100 transition hover:cursor-pointer group-hover:scale-[1.1] group-hover:translate-y-[-0.9vh]"
       ></img>
     </div>
     // </div>
